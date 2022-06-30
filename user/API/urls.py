a10a306217f43d7b40898fb8cc8eec0dd37e028d
sudonnoh <sudonnoh@gmail.com>
2022-06-30 16:59:47 +0900

    update

--- user/API/urls.py
+++ user/API/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from user.API.views import (
-    NaverLoginAPIView, NaverCallbackAPIView
+    NaverLoginAPIView, NaverCallbackAPIView, NaverToDjangoLoginView
     )
 
 urlpatterns = [
     path('naver/login', NaverLoginAPIView.as_view()),
     path('naver/callback', NaverCallbackAPIView.as_view()),
+    path('naver/login/success', NaverToDjangoLoginView.as_view()),
 ]
--- user/API/views.py
+++ user/API/views.py
@@ -1,10 +1,22 @@
 # settings.py 에서 설정한 MAIN_DOMAIN 등을 불러오기 위해 import 함
+from json import JSONDecodeError
+
 from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import redirect
 
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
+import requests
+
+from user.models import User
+
+from allauth.socialaccount.providers.naver import views as naver_views
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
 
 # main domain(http://127.0.0.1:8000)
 main_domain = settings.MAIN_DOMAIN
@@ -36,6 +48,75 @@ class NaverCallbackAPIView(APIView):
     
     def get(self, request, *args, **kwargs):
         try:
-            print("성공")
+            # Naver Login Parameters
+            grant_type = 'authorization_code'
+            client_id = settings.NAVER_CLIENT_ID
+            client_secret = settings.NAVER_CLIENT_SECRET
+            code = request.GET.get('code')
+            state = request.GET.get('state')
+
+            parameters = f"grant_type={grant_type}&client_id={client_id}&client_secret={client_secret}&code={code}&state={state}"
+
+            # token request
+            token_request = requests.get(
+                f"https://nid.naver.com/oauth2.0/token?{parameters}"
+            )
+
+            token_response_json = token_request.json()
+            error = token_response_json.get("error", None)
+
+            if error is not None:
+                raise JSONDecodeError(error)
+
+            access_token = token_response_json.get("access_token")
+
+            # User info get request
+            user_info_request = requests.get(
+                "https://openapi.naver.com/v1/nid/me",
+                headers={"Authorization": f"Bearer {access_token}"},
+            )
+
+            # User 정보를 가지고 오는 요청이 잘못된 경우
+            if user_info_request.status_code != 200:
+                return JsonResponse({"error": "failed to get email."}, status=status.HTTP_400_BAD_REQUEST)
+
+            user_info = user_info_request.json().get("response")
+            email = user_info["email"]
+
+            # User 의 email 을 받아오지 못한 경우
+            if email is None:
+                return JsonResponse({
+                    "error": "Can't Get Email Information from Naver"
+                }, status=status.HTTP_400_BAD_REQUEST)
+
+            try:
+                user = User.objects.get(email=email)
+                data = {'access_token': access_token, 'code': code}
+                # accept 에는 token 값이 json 형태로 들어온다({"key"}:"token value")
+                # 여기서 오는 key 값은 authtoken_token에 저장된다.
+                accept = requests.post(
+                    f"{main_domain}/user/naver/login/success", data=data
+                )
+                # 만약 token 요청이 제대로 이루어지지 않으면 오류처리
+                if accept.status_code != 200:
+                    return JsonResponse({"error": "Failed to Signin."}, status=accept.status_code)
+                return Response(accept.json(), status=status.HTTP_200_OK)
+                # return JsonResponse(accept.json())
+
+            except User.DoesNotExist:
+                data = {'access_token': access_token, 'code': code}
+                accept = requests.post(
+                    f"{main_domain}/user/naver/login/success", data=data
+                )
+                # token 발급
+                return Response(accept.json(), status=status.HTTP_200_OK)
+                # return JsonResponse(accept.json())
         except:
-            print("실패")
+            return JsonResponse({
+                "error": "error",
+            }, status=status.HTTP_404_NOT_FOUND)
+
+
+class NaverToDjangoLoginView(SocialLoginView):
+    adapter_class = naver_views.NaverOAuth2Adapter
+    client_class = OAuth2Client
